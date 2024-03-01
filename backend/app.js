2e6af4311f9f30cb6bb466041dc3fc72f69a918c
induwarafernando <induwarafernando9@gmail.com>
2024-03-01 21:13:10 +0530

    setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()   
+
 
 require('dotenv').config()
 
 const  PORT = process.env.PORT
 
-const server = () => {
+app.use(express.json())
+app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const server = () => {
+    app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`)
+    })
 }
 
 server()
