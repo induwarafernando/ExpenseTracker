bcc93f05f587f67fdffad05eb058ba0d142512e0
induwarafernando <induwarafernando9@gmail.com>
2024-03-03 22:03:34 +0530

    transaction

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -6,6 +6,9 @@ const router = require('express').Router();
 
 router.post('/add-income',addIncome)
 
+    .get('/get-incomes',getIncomes)
+    .delete('/delete-income/:id',deleteIncome)
+
 
 module.exports = router;
 
