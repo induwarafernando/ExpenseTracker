383f6be27ad631a300e25ec5ae0fb0ff5bc3f381
induwarafernando <induwarafernando9@gmail.com>
2024-04-04 22:11:49 +0530

    Using React Components -created dashboard

---
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Dashboard() {
+    return (
+        <div>
+            <DashboardStyled>Dashboard</DashboardStyled>
+        </div>
+    )
+}
+
+const DashboardStyled = styled.div`
+
+`;
+
+export default Dashboard
