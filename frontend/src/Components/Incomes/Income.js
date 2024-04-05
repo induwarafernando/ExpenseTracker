eac3a0152c83311ed0c864783579617860ec7a39
induwarafernando <induwarafernando9@gmail.com>
2024-04-05 09:16:44 +0530

    Income component

--- frontend/src/Components/Incomes/Income.js
+++ frontend/src/Components/Incomes/Income.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import { InnerLayout } from '../../styles/Layouts';
 
 function Income() {
     return (
         <div>
-            <IncomeStyled>Income</IncomeStyled>
+            <IncomeStyled>
+                <InnerLayout>
+                    <h1>Income</h1>
+                    <div className="income-content">
+                        <div className='form-container'></div>
+                        <div className='incomes'>
+                            
+                        </div>
+                    </div>
+                </InnerLayout>
+            </IncomeStyled>
         </div>
     )
 }
