8482639d817bde5341fc71e900831b44c79d6cda
induwarafernando <induwarafernando9@gmail.com>
2024-04-06 12:57:40 +0530

    form

---
+++ frontend/src/Components/Form/Form.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+
+function Form() {
+  const [inputState, setInputState] = useState({
+     title: '',
+     amount: '',
+     date: '',
+     category: '',
+     description: '',
+    });
+
+const {title, amount, date, category, description} = inputState;
+const handleInput = name => e => {
+    setInputState({
+        ...inputState,
+        [name]: e.target.value
+    });
+}   
+
+return (
+      <FormStyled>
+        <div className="input-control">
+            <input 
+                type="text" 
+                value={title}
+                name="title"
+                placeholder="Salary Title"
+                onChange={handleInput('title')}
+                />
+        </div>
+        <div className="input-control">
+            <input 
+                type="text" 
+                value={amount}
+                name="amount"
+                placeholder="Salary Amount"
+                onChange={handleInput('amount')}
+                />
+        </div>
+        <div className="input-control">
+            <DatePicker 
+                id="date"
+                placeholderText="Enter a Date" 
+                selected={date}
+                dateFormat={"dd/MM/yyyy"}
+                onChange={(date) => setInputState({...inputState, date} 
+                )}
+            />       
+        </div>
+
+        <div className="input-control">
+            <select required value = {category} name="category" id="category" onChange={handleInput('category')}>
+                <option value="" disabled>Select Option</option>
+                
+                //salary,freelancing,investment,stocks,bitcoin,bank,youtube,other
+                <option value="salary">Salary</option>
+                <option value="freelancing">Freelancing</option>
+                <option value="investment">Investment</option>
+                <option value="stocks">Stocks</option>
+                <option value="bitcoin">Bitcoin</option>
+                <option value="bank">Bank</option>
+                <option value="youtube">Youtube</option>
+                <option value="other">Other</option>
+            </select>
+        </div>
+
+      </FormStyled>
+  );
+}
+
+const FormStyled = styled.div``;    
+export default Form;
